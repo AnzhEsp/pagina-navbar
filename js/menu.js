d47e73517bc92a565bcfab94bfd59c0ac7502850
AnzhEsp <aldair.espino@yahoo.es>
2022-05-23 16:56:12 -0500

    aldair

--- js/menu.js
+++ js/menu.js
@@ -12,11 +12,7 @@ const menuItem = document.querySelectorAll('.menu__toggle')
      })
    })
  }
-// item.addEventListener('click',()=>{
-//       item.lastElementChild.classList.toggle('slide')
-//       const div=item.firstElementChild
-//       div.classList.toggle('menu__item-text')
-//      })
+
   window.addEventListener('resize',()=>{
     if(window.innerWidth <= 768){
     addClick()
