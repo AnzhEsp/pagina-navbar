70f2a283afe7a9755e6a98e65fb0dc46635fa652
AnzhEsp <aldair.espino@yahoo.es>
2022-05-23 15:53:42 -0500

    first commit

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,21 @@
 
 
 
-(function() {
-const menuItem = document.querySelectorAll('.menu__toggle')
- const addClick=()=>{
-   menuItem.forEach(item=>{
-     item.addEventListener('click',()=>{
-      item.lastElementChild.classList.toggle('slide')
-      const div=item.firstElementChild
-      div.classList.toggle('menu__item-text')
-     })
-   })
- }
+// (function() {
+// const menuItem = document.querySelectorAll('.menu__toggle')
+//  const addClick=()=>{
+//    menuItem.forEach(item=>{
+//      item.addEventListener('click',()=>{
+//       item.lastElementChild.classList.toggle('slide')
+//       const div=item.firstElementChild
+//       div.classList.toggle('menu__item-text')
+//      })
+//    })
+//  }
 
-  window.addEventListener('resize',()=>{
-    if(window.innerWidth <= 768){
-    addClick()
-    }
-  })
-})();
+//   window.addEventListener('resize',()=>{
+//     if(window.innerWidth <= 768){
+//     addClick()
+//     }
+//   })
+// })();
