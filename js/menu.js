6ca841e54d0124070b99be4fea28f57ad42a25b7
AnzhEsp <aldair.espino@yahoo.es>
2022-05-23 16:25:47 -0500

    se corrigio el nav

--- js/menu.js
+++ js/menu.js
@@ -5,14 +5,18 @@
 const menuItem = document.querySelectorAll('.menu__toggle')
  const addClick=()=>{
    menuItem.forEach(item=>{
-     item.addEventListener('click',()=>{
-      item.lastElementChild.classList.toggle('slide')
-      const div=item.firstElementChild
-      div.classList.toggle('menu__item-text')
+     item.firstElementChild.firstElementChild.addEventListener('click',()=>{
+       item.lastElementChild.classList.toggle('slide')
+       item.firstElementChild.classList.toggle('menu__item-text')
+       
      })
    })
  }
-
+// item.addEventListener('click',()=>{
+//       item.lastElementChild.classList.toggle('slide')
+//       const div=item.firstElementChild
+//       div.classList.toggle('menu__item-text')
+//      })
   window.addEventListener('resize',()=>{
     if(window.innerWidth <= 768){
     addClick()
